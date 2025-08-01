9ad087a70b54caef1f84019d98e2648819ae6295
Ryan Carver <ryan@ryancarver.com>
2025-08-01 07:59:52 -0400

    Initial Commit

---
+++ TCA Observation Test/TCA_Observation_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Observation_TestApp.swift
+//  TCA Observation Test
+//
+//  Created by Ryan Carver on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Observation_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
